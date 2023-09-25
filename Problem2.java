7ec57dfbca5419b1bf3418fe909fab4c26eab098
Arthur Both <arthur.both001@edu.pucrs.br>
2023-09-25 12:22:20 -0300

    Criação dos arquivos-base

---
+++ Problem2.java
@@ -0,0 +1,11 @@
+public class Problem2 {
+
+    /*
+     Até 1969 matemáticos acreditavam sem impossível resolver um problema de multiplicação de matrizes em
+        tempo menor que Θ(n^3). Procure pelos trabalhos de V. Strassen e responda se isso ainda é verdade.
+     */
+    
+    public Problem2() {
+        
+    }
+}
